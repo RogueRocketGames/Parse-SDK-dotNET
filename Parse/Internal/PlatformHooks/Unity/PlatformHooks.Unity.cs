249f32e0bb2ff15524e1a63b2f1cd03533799173
scritch <rsun@roguerocketgames.com>
2020-05-19 20:06:33 -0700

    parse - update for unity 2019.3

--- Parse/Internal/PlatformHooks/Unity/PlatformHooks.Unity.cs
+++ Parse/Internal/PlatformHooks/Unity/PlatformHooks.Unity.cs
@@ -17,8 +17,14 @@ using UnityEngine;
 namespace Parse {
 
     //hack
+    ///<Summary>
+    /// UnityOverrides, added by Rogue Rocket Games for more control from unity
+    ///</Summary>
     public class UnityOverrides
     {
+        ///<Summary>
+        /// UnityOverrides, added by Rogue Rocket Games for more control from unity: provide persistent folder path
+        ///</Summary>
         public static string sOverridePersistentPath = null;
     }
 
@@ -190,7 +196,7 @@ namespace Parse {
         if (settingsPath == null) {
           throw new InvalidOperationException("Parse must be initialized before making any calls.");
         }
-        return Application.platform == RuntimePlatform.WP8Player;
+        return false;
       }
     }
 
@@ -1175,10 +1181,10 @@ namespace Parse {
       settingsPath = Path.Combine(path, "Parse.settings");
       // We can only set some values here since we can be sure that Initialize is always called
       // from main thread.
-      isWebPlayer = Application.isWebPlayer;
+      isWebPlayer = false;
       osVersion = SystemInfo.deviceModel;
       appBuildVersion = Application.version;
-      appDisplayVersion = Application.bundleIdentifier;
+      appDisplayVersion = Application.identifier;
       appName = Application.productName;
 
       settings = SettingsWrapper.Wrapper;
